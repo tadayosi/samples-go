1a23801b1895b6c6459919f074f062dfcc1f71f0
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2021-10-18 21:54:42 +0900

    gomega retry

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/avast/retry-go v3.0.0+incompatible
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/mitchellh/mapstructure v1.4.0
+	github.com/onsi/gomega v1.16.0 // indirect
 	github.com/stretchr/testify v1.6.1
 )
--- test/retry_test.go
+++ test/retry_test.go
@@ -7,8 +7,17 @@ import (
 	"time"
 
 	"github.com/avast/retry-go"
+	. "github.com/onsi/gomega"
 	"github.com/stretchr/testify/assert"
 )
+
+func flaky() error {
+	i := rand.Intn(3)
+	if i > 0 {
+		return fmt.Errorf("i = %d", i)
+	}
+	return nil
+}
 
 func TestRetry(t *testing.T) {
 	rand.Seed(time.Now().UnixNano())
@@ -16,11 +25,23 @@ func TestRetry(t *testing.T) {
 	err := retry.Do(func() error {
 		count++
 		fmt.Printf("count = %d\n", count)
-		i := rand.Intn(3)
-		if i > 0 {
-			return fmt.Errorf("i = %d", i)
-		}
+		return flaky()
+	}, retry.Attempts(10))
+	assert.Nil(t, err)
+}
+
+func TestGomegaRetry(t *testing.T) {
+	rand.Seed(time.Now().UnixNano())
+
+	g := NewGomegaWithT(t)
+
+	count := 0
+	err := retry.Do(func() error {
+		count++
+		fmt.Printf("count = %d\n", count)
+		g.Expect(flaky).To(BeNil())
 		return nil
-	})
+	}, retry.Attempts(10))
+
 	assert.Nil(t, err)
 }

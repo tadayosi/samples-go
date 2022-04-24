33e1522c9be2a57a07ae5b2e7b54f1888c1c5f8b
Tadayoshi Sato <sato.tadayoshi@gmail.com>
2022-04-24 19:07:05 +0900

    mapper: add composite struct tests

--- mapper/mapper_test.go
+++ mapper/mapper_test.go
@@ -8,12 +8,19 @@ import (
 
 	"github.com/mitchellh/mapstructure"
 	"github.com/stretchr/testify/assert"
+
+	. "github.com/tadayosi/samples-go/util"
 )
 
 type Object struct {
 	PropOne   *bool  `property:"prop-one" json:"propOne,omitempty"`
 	PropTwo   int    `property:"prop-two" json:"propTwo,omitempty"`
 	PropThree string `property:"prop-three" json:"propThree,omitempty"`
+}
+
+type Child struct {
+	Object   `property:",squash"`
+	PropFour string `property:"prop-four" json:"propFour,omitempty"`
 }
 
 func (o *Object) String() string {
@@ -28,7 +35,7 @@ func TestMap2Object(t *testing.T) {
 		"prop-three": "test",
 	}
 	obj := &Object{
-		PropOne:   &[]bool{true}[0],
+		PropOne:   BoolP(true),
 		PropTwo:   0,
 		PropThree: "",
 	}
@@ -50,9 +57,44 @@ func TestMap2Object(t *testing.T) {
 	assert.Equal(t, "test", obj.PropThree)
 }
 
+func TestMap2Child(t *testing.T) {
+	data := map[string]interface{}{
+		"prop-one":   "false",
+		"prop-two":   "1",
+		"prop-three": "test",
+		"prop-four":  "child-test",
+	}
+	child := &Child{
+		Object: Object{
+			PropOne:   BoolP(true),
+			PropTwo:   0,
+			PropThree: "",
+		},
+		PropFour: "",
+	}
+
+	decoder, err := mapstructure.NewDecoder(
+		&mapstructure.DecoderConfig{
+			Metadata:         &mapstructure.Metadata{},
+			WeaklyTypedInput: true,
+			TagName:          "property",
+			Result:           child,
+			ErrorUnused:      true,
+		},
+	)
+	assert.Nil(t, err)
+
+	err = decoder.Decode(data)
+	assert.Nil(t, err)
+	assert.False(t, *child.PropOne)
+	assert.Equal(t, 1, child.PropTwo)
+	assert.Equal(t, "test", child.PropThree)
+	assert.Equal(t, "child-test", child.PropFour)
+}
+
 func TestObject2JSON(t *testing.T) {
 	obj := &Object{
-		PropOne:   &[]bool{false}[0],
+		PropOne:   BoolP(false),
 		PropTwo:   1,
 		PropThree: "test",
 	}
@@ -63,9 +105,25 @@ func TestObject2JSON(t *testing.T) {
 	assert.JSONEq(t, `{"propOne":false,"propTwo":1,"propThree":"test"}`, string(data))
 }
 
+func TestChild2JSON(t *testing.T) {
+	child := &Child{
+		Object: Object{
+			PropOne:   BoolP(false),
+			PropTwo:   1,
+			PropThree: "test",
+		},
+		PropFour: "child-test",
+	}
+
+	data, err := json.Marshal(child)
+
+	assert.Nil(t, err)
+	assert.JSONEq(t, `{"propOne":false,"propTwo":1,"propThree":"test","propFour":"child-test"}`, string(data))
+}
+
 func TestObject2JSON_PropOneEmpty(t *testing.T) {
 	obj := &Object{
-		//PropOne:   &[]bool{false}[0],
+		//PropOne:   BoolP(false),
 		PropTwo:   1,
 		PropThree: "test",
 	}
@@ -84,7 +142,7 @@ func TestMap2Object2JSON(t *testing.T) {
 		"prop-three": "test",
 	}
 	o := &Object{
-		PropOne:   &[]bool{true}[0],
+		PropOne:   BoolP(true),
 		PropTwo:   0,
 		PropThree: "",
 	}
@@ -107,6 +165,42 @@ func TestMap2Object2JSON(t *testing.T) {
 
 	assert.Nil(t, err)
 	assert.JSONEq(t, `{"propOne":false,"propTwo":1,"propThree":"test"}`, string(result))
+}
+
+func TestMap2Child2JSON(t *testing.T) {
+	data := map[string]interface{}{
+		"prop-one":   "false",
+		"prop-two":   "1",
+		"prop-three": "test",
+		"prop-four":  "child-test",
+	}
+	c := &Child{
+		Object: Object{
+			PropOne:   BoolP(true),
+			PropTwo:   0,
+			PropThree: "",
+		},
+		PropFour: "",
+	}
+	child := reflect.New(reflect.TypeOf(c)).Interface()
+
+	decoder, err := mapstructure.NewDecoder(
+		&mapstructure.DecoderConfig{
+			Metadata:         &mapstructure.Metadata{},
+			WeaklyTypedInput: true,
+			TagName:          "property",
+			Result:           child,
+		},
+	)
+	assert.Nil(t, err)
+
+	err = decoder.Decode(data)
+	assert.Nil(t, err)
+
+	result, err := json.Marshal(child)
+
+	assert.Nil(t, err)
+	assert.JSONEq(t, `{"propOne":false,"propTwo":1,"propThree":"test","propFour":"child-test"}`, string(result))
 }
 
 func TestJSON2Object(t *testing.T) {
---
+++ util/util.go
@@ -0,0 +1,5 @@
+package util
+
+func BoolP(b bool) *bool {
+	return &b
+}
